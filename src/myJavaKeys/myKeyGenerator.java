5c13b2a383f5f2f166a49e4b260d5cd5230e2bf1
Sabin Thomas <sabin@codiscope.com>
2016-04-08 15:36:05 -0400

    First commit for Key generators in Java

---
+++ src/myJavaKeys/myKeyGenerator.java
@@ -0,0 +1,40 @@
+package myJavaKeys;
+
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.spec.RSAKeyGenParameterSpec;
+import java.security.spec.RSAPublicKeySpec;
+
+public class myKeyGenerator {
+	
+	public static void main(String[] args) throws Exception{
+		int eValue = 79;
+		int bitLength = 1024;
+		
+		BigInteger e = e = new BigInteger(Integer.toString(eValue));
+		
+		KeyPairGenerator myKeyGen = KeyPairGenerator.getInstance("RSA");
+		myKeyGen.initialize(bitLength);
+		KeyPair kp = myKeyGen.generateKeyPair();
+		
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		RSAPublicKeySpec kSpec = (RSAPublicKeySpec) kf.getKeySpec(kp.getPublic(),RSAPublicKeySpec.class);
+		
+		myKeyGen = KeyPairGenerator.getInstance("RSA", "SunRsaSign");
+		
+		e = new BigInteger(Integer.toString(eValue));
+		System.out.println("e=" + e);
+		RSAKeyGenParameterSpec param = new RSAKeyGenParameterSpec(bitLength, e);
+		myKeyGen.initialize(param);
+		kp = myKeyGen.generateKeyPair();
+		
+		kf = KeyFactory.getInstance("RSA","SunRsaSign");
+		
+		kSpec = (RSAPublicKeySpec) kf.getKeySpec(kp.getPublic(), RSAPublicKeySpec.class);
+		
+		
+	}
+
+}
